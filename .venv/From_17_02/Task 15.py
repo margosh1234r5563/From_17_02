ea50cec3a81c960198cc97c7839dc16a03521d20
margosh1234r5563 <m.rusakova@g.nsu.ru>
2024-02-11 17:52:50 +0700

    Task 14

--- .venv/From_17_02/Task 15.py
+++ .venv/From_17_02/Task 15.py
@@ -1,9 +1,9 @@
-#Task 15
+# Task 15
 cm = float(input('Введите расстояние в сантиметрах: '))
 d = cm / 2.54
 f = cm / (2.54 * 12)
 yard = cm / (2.54 * 12 * 3)
-miles = cm / (2.54 *12 * 3 * 1760)
+miles = cm / (2.54 * 12 * 3 * 1760)
 print(yard, 'ярдов')
 print(miles, 'мили')
 print(f, 'футов')
