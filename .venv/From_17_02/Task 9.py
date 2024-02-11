2ac551a3016aa409b1b259b9ed3931e6c32676f7
margosh1234r5563 <m.rusakova@g.nsu.ru>
2024-02-11 15:24:03 +0700

    Task 1

---
+++ .venv/From_17_02/Task 9.py
@@ -0,0 +1,8 @@
+#Task 9
+print('Введите плей-лист папы:')
+first=input()
+second=input()
+third=input()
+fourt=input()
+fifeth=input()
+print('Плей-лист мамы:',fifeth,fourt,third,second,first,sep='\n')
