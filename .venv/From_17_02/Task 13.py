398e6ab6b198ace86058cfe0497d7ab3b35362f0
margosh1234r5563 <m.rusakova@g.nsu.ru>
2024-02-13 16:47:18 +0700

    Task 6

--- .venv/From_17_02/Task 13.py
+++ .venv/From_17_02/Task 13.py
@@ -1,11 +1,8 @@
 # Task 13
 import math as m
-
-print('Введите радиус "слепой зоны"')
-r_small_zone = int(input())
-print('Введите радиус "дальности приема"')
-r_big_zone = int(input())
-s_small_zone = m.pi * r_small_zone ** 2
-s_big_sone = m.pi * r_big_zone ** 2
-s_cover = s_big_sone - s_small_zone
-print('Площадь покрываемой территории', s_cover)
+r_1_zone = int(input('Введите радиус первой зоны: '))
+r_2_zone = int(input('Введите радиус второй зоны: '))
+s_1_zone = m.pi * r_1_zone ** 2
+s_2_zone = m.pi * r_2_zone ** 2
+s_cover = s_2_zone - s_1_zone
+print('Площадь покрываемой территории', abs(s_cover))
