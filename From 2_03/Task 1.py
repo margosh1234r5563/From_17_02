deb400238e39af438b840dec150e43b792fc0dfe
margosh1234r5563 <m.rusakova@g.nsu.ru>
2024-02-14 00:13:05 +0700

    Task 1

---
+++ From 2_03/Task 1.py
@@ -0,0 +1,12 @@
+# Task 1
+total_cost = int(input('Введите общую стоимость часов:'))
+amount_silver_clock = 96
+amount_golden_clock = amount_silver_clock / 16
+price_silver_clock = 48
+cost_silver = amount_silver_clock * price_silver_clock
+cost_golden_clock = total_cost - cost_silver
+price_golden_clock = cost_golden_clock / amount_golden_clock
+if int(price_golden_clock) > 0:
+    print(int(price_golden_clock))
+else:
+    print('Цена золотых часов не может быть отрицательной')
