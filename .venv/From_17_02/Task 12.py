1f33164b19bc27eeca471f3cfb1065680e9012e3
margosh1234r5563 <m.rusakova@g.nsu.ru>
2024-02-13 16:25:22 +0700

    Task 12

--- .venv/From_17_02/Task 12.py
+++ .venv/From_17_02/Task 12.py
@@ -1,10 +1,12 @@
 # Task 12
-print('Введите общую стоимость часов')
-total_cost = int(input())
+total_cost = int(input('Введите общую стоимость часов: '))
 amount_silver_clock = 96
 amount_golden_clock = amount_silver_clock / 16
 price_silver_clock = 48
 cost_silver = amount_silver_clock * price_silver_clock
 cost_golden_clock = total_cost - cost_silver
 price_golden_clock = cost_golden_clock / amount_golden_clock
-print(price_golden_clock)
+if price_golden_clock > 0:
+    print(price_golden_clock)
+else:
+    print('Цена золотых часов не может быть отрицательной')
