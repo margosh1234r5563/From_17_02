27683c96967db62bfa3107d2a052f4d9644a5c74
margosh1234r5563 <m.rusakova@g.nsu.ru>
2024-02-14 00:13:36 +0700

    Task 1

---
+++ From 2_03/Task 4.py
@@ -0,0 +1,8 @@
+# Task 4
+count = 0
+for i in range(1, 8):
+    for i in range(1, 8):
+        for i in range(1, 8):
+            for i in range(1, 8):
+                count = count + 1
+print(count)
