18ed29983cadd588b6753c716163adbf68d7aa5b
margosh1234r5563 <m.rusakova@g.nsu.ru>
2024-04-18 22:46:01 +0700

    Task 1

--- From 20_04/Task 7.py
+++ From 20_04/Task 7.py
@@ -1,7 +1,33 @@
-for i in range(1000, 9999 + 1):
-    for b in range(1000, 9999 + 1):
-        if (i + b) < 99999 and (i + b) > 9999 and (i + b) // 10000 == b // 1000 and (
-                i + b) // 1000 % 10 == b // 100 % 10 and i // 100 % 10 == b % 10 % 10 % 10 == (
-                i + b) // 10 % 10 % 10 and i // 10 % 10 % 10 == (i + b) // 10 % 10 % 10 and i // 10 % 10 % 10 == (
-                i + b) // 100 % 10:
-            print(i, b, i + b)
+for S in range(1, 10):
+    for E in range(0, 10):
+        if E == S:
+            continue
+        for N in range(0, 10):
+            if N in [E, S]:
+                continue
+            for D in range(0, 10):
+                if D in [N, E, S]:
+                    continue
+                for M in range(1, 10):
+                    if M in [D, N, E, S]:
+                        continue
+                    for O in range(0, 10):
+                        if O in [M, D, N, E, S]:
+                            continue
+                        for R in range(0, 10):
+                            if R in [O, M, D, N, E, S]:
+                                continue
+                            SEND = S * 1000 + E * 100 + N * 10 + D
+                            MORE = M * 1000 + O * 100 + R * 10 + E
+                            if 1000 <= SEND <= 9999 and 1000 <= MORE <= 9999:
+                                MONEY = SEND + MORE
+                                Y = MONEY % 10
+                                M_1 = MONEY // 10000
+                                O_1 = MONEY // 1000 % 10
+                                N_1 = MONEY // 100 % 10
+                                E_1 = MONEY // 10 % 10
+                                if M_1 == M and O_1 == O and N_1 == N and E_1 == E:
+                                    if len(set(str(MONEY))) == 5:
+                                        print(f'SEND = {SEND}')
+                                        print(f'MORE = {MORE}')
+                                        print(f'MONEY = {MONEY}')
